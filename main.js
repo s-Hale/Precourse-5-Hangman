a4bc58ab40343f9b388e796eebe703b37c045f49
sHale <sally.hale@homeport.org.uk>
2017-10-13 14:27:43 +0100

    Compare and replace selected letter

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
     var chosenWord;
     var underscores = [];
     var guesses = [];
+    var userLetter = "";
+    var checkWordSplit = [];
 
     //generate an array of underscores of the right length
 
@@ -12,6 +14,25 @@ $(document).ready(function() {
             underscores.push('_');
         })
         return underscores;
+    }
+
+    //split the word into an array and compare individual letters
+
+    function isLetterPresent(userLetter) {
+        userLetter = userLetter.toLowerCase();
+        checkWordSplit = chosenWord.split('');
+        var result = false;
+
+        //reassign any correct letters into the array of underscores and update DOM
+
+        for (var i = 0; i < checkWordSplit.length; i++) {
+            if (checkWordSplit[i] === userLetter) {
+                result = true;
+                guesses[i] = userLetter;
+                $("#wordSpace").html(guesses);
+
+            }
+        }
     }
 
 
@@ -23,5 +44,19 @@ $(document).ready(function() {
         guesses = generateUnderscores(chosenWord);
         $(event.target).addClass("hidden");
         $("#wordSpace").removeClass("hidden").html(guesses);
+    });
+
+    //to avoid 26 click handlers, used child elements - spans - and therefore any click within alphabet keys includes
+    //children; target selects individual span. Had to ensure entire element couldn't be selected.
+    //took text from the target span to use to compare.
+
+    $("#alphabetKeys").on("click", function(event) {
+        var target = $(event.target);
+        if (event.target !== event.currentTarget) {
+            target.addClass("selected");
+            var wellIsIt = "";
+            userLetter = target.text();
+            wellIsIt = isLetterPresent(userLetter);
+        }
     });
 });
