f3b66a3c62f830e10b2c6e3ad670ef390d3425f0
sHale <sally.hale@homeport.org.uk>
2017-10-15 19:13:36 +0100

    Categories, effects, styling

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
-    var wordDatabase = ["gingerbread", "pizza", "sausage", "cauliflower", "potato", "cheese", "chocolate"];
+    var foodDatabase = ["gingerbread", "pizza", "sausage", "cauliflower", "potato", "cheese", "chocolate"];
+    var geoDatabase = ["japan", "spain", "germany", "denmark", "finland", "iceland", "norway", "mexico", "china", "luxembourg", "ukraine", "canada", "india", "australia"];
+    var animalDatabase = ["crocodile", "octopus", "giraffe", "hedgehog", "otter", "hare", "ladybird", "peacock", "capybara", "possum"];
+    var mythDatabase = ["persephone", "zeus", "artemis", "aphrodite", "poseidon", "dionysus", "apollo", "olympia", "selene", "hades"];
+    var minecraftDatabase = ["creeper", "enderman", "redstone", "shulker", "mooshroom", "ghast", "wither"];
+    var selectedDatabase = foodDatabase;
 
     var chosenWord;
-    var underscores = [];
-    var guesses = [];
     var userLetter = "";
     var checkWordSplit = [];
     var counter = 0;
+    var underscores = [];
+    var guesses = [];
     var gingerbreads = [$("#ging1"), $("#ging2"), $("#ging3"), $("#ging4")];
 
     //generate an array of underscores of the right length
@@ -26,6 +31,7 @@ $(document).ready(function() {
         var result = false;
 
         //reassign any correct letters into the array of underscores and update DOM
+        //stop users clicking once word is solved
 
         for (var i = 0; i < checkWordSplit.length; i++) {
             if (checkWordSplit[i] === userLetter) {
@@ -34,6 +40,7 @@ $(document).ready(function() {
                 $("#wordSpace").html(guesses);
                 if (guesses.join("") === chosenWord) {
                     $("#wordSpace").addClass("aWinnerIsYou");
+                    $("#alphabetKeys").addClass("noClick");
                 }
             }
         }
@@ -53,19 +60,81 @@ $(document).ready(function() {
         return result;
     }
 
+    //reset
+
+    function reset() {
+        $("#alphabetKeys").addClass("noClick");
+        $("#wordSpace").addClass("hidden").removeClass("aWinnerIsYou");
+        $("#startButton").removeClass("hidden");
+        $("#alphabetKeys span").removeClass("selected");
+        chosenWord = "";
+        userLetter = "";
+        checkWordSplit = [];
+        counter = 0;
+        underscores = [];
+        guesses = [];
+        gingerbreads[1].addClass("hidden");
+        gingerbreads[2].addClass("hidden");
+        gingerbreads[3].addClass("hidden");
+        gingerbreads[0].removeClass("hidden");
+    }
+
+    //choosing a category
+
+    $("#foodIcon").on("click", function() {
+        reset();
+        $("#foodIcon")
+            .addClass("selectedIcon")
+            .siblings()
+            .removeClass("selectedIcon");
+        selectedDatabase = foodDatabase;
+    });
+    $("#geoIcon").on("click", function() {
+        reset();
+        $("#geoIcon")
+            .addClass("selectedIcon")
+            .siblings()
+            .removeClass("selectedIcon");
+        selectedDatabase = geoDatabase;
+    });
+    $("#animalIcon").on("click", function() {
+        reset();
+        $("#animalIcon")
+            .addClass("selectedIcon")
+            .siblings()
+            .removeClass("selectedIcon");
+        selectedDatabase = animalDatabase;
+    });
+    $("#mythIcon").on("click", function() {
+        reset();
+        $("#mythIcon")
+            .addClass("selectedIcon")
+            .siblings()
+            .removeClass("selectedIcon");
+        selectedDatabase = mythDatabase;
+    });
+    $("#minecIcon").on("click", function() {
+        reset();
+        $("#minecIcon")
+            .addClass("selectedIcon")
+            .siblings()
+            .removeClass("selectedIcon");
+        selectedDatabase = minecraftDatabase;
+    });
 
     //let's begin!
-    //choose a word, generate an array of underscores
+    //users cannot click letters until start button pressed.
+    //choose a word, generate an array of underscores.
 
     $("#startButton").on("click", function(event) {
-        chosenWord = wordDatabase[Math.floor(Math.random() * wordDatabase.length)];
+        $("#alphabetKeys").removeClass("noClick");
+        chosenWord = selectedDatabase[Math.floor(Math.random() * selectedDatabase.length)];
         guesses = generateUnderscores(chosenWord);
         $(event.target).addClass("hidden");
         $("#wordSpace").removeClass("hidden").html(guesses);
     });
 
-    //to avoid 26 click handlers, used child elements - spans - and therefore any click within alphabet keys includes
-    //children; target selects individual span. Had to ensure entire element couldn't be selected.
+    //to avoid 26 click handlers, used child elements - spans - and therefore any click within alphabet keys includes children; target selects individual span. Had to ensure entire element couldn't be selected.
     //took text from the target span to use to compare.
 
     $("#alphabetKeys").on("click", function(event) {
@@ -76,5 +145,9 @@ $(document).ready(function() {
             userLetter = target.text();
             wellIsIt = isLetterPresent(userLetter);
         }
+    });
+
+    $("#resetButton").on("click", function() {
+        reset();
     });
 });
